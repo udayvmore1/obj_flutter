c0f06c4ca8987d823f588a051ee512af208384db
udayvmore1 <udayvmore1@gmail.com>
2021-06-09 12:10:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:object_detection/HomeScreen.dart';
+
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error $e.code \n Error Message : $e.message');
+  }
+  runApp(new MainScreen());
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(cameras),
+    );
+  }
+}
